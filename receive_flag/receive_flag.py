bde000b5c10e71a514ac0866ca42d5dde4ffcf89
ripples <564311873@qq.com>
2015-12-23 22:12:00 +0800

    Improve receive flag.

--- receive_flag/receive_flag.py
+++ receive_flag/receive_flag.py
@@ -4,6 +4,7 @@
 import SocketServer
 import requests
 import time
+import json
 
 # This function should be replaced.
 def submit_flag(flag):
@@ -24,7 +25,7 @@ def submit_with_retry(flag):
             if type(result) is bool:
                 return result
         except Exception, e:
-            raise e
+            result = e.message
         retry += 1
         time.sleep(1)
     return result
@@ -34,17 +35,32 @@ def log(filename, msg):
     fp.write('[%s] %s\n' % (time.asctime(), str(msg).replace('\n', '; ')))
     fp.close()
 
+def record_in_db(info):
+    print 'Will record in database:', info
+
 class FlagHandler(SocketServer.StreamRequestHandler):
     def handle(self):
-        flag = self.rfile.read().strip()
+        content = self.rfile.read().strip()
         ip = self.client_address[0]
-        result = submit_with_retry(flag)
+
+        try:
+            info = json.loads(content)
+            assert info['flag'] and info['service']
+        except Exception, e:
+            info = {'flag': content, 'service': None}
+
+        result = submit_with_retry(info['flag'])
+        if 'id' not in info:
+            info['id'] = ip
+        info['result'] = result
+        record_in_db(info)
+
         if result is True:
-            log('out.log', '(%s) %s' % (ip, flag))
+            log('out.log', '(%s) %s' % (ip, content))
             self.wfile.write('Success')
         elif result is not False:
-            log('error.log', '(%s) %s => %s' % (ip, flag, result))
-            self.wfile.write('Error: %r, Flag: %r' % (result, flag))
+            log('error.log', '(%s) %s => %s' % (ip, content, result))
+            self.wfile.write('Error: %r, Submit: %r' % (result, content))
         else:
             self.wfile.write('Duplicate')
 
